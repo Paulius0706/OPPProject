b5c86e2a403dff2cb19821362d93942d869e96a1
Aurvas <79143711+Aurvas@users.noreply.github.com>
2022-11-03 17:10:27 +0200

    Singleton fix v2

--- BlazorGame1/BlazorGame/BlazorGame/Game/GameObjects/Factories/BaseFactory.cs
+++ BlazorGame1/BlazorGame/BlazorGame/Game/GameObjects/Factories/BaseFactory.cs
@@ -13,14 +13,14 @@ namespace BlazorGame.Game.GameObjects.Factories
         public override BulletObject CreateBulletObject()
         {
             BulletBuilder bulletBuilder = new BulletBuilder(new float[] { 0, 0 }, new float[] { 0, 0 });
-            Director.director.Construct(ref bulletBuilder, gameObject.id, player.damage, player.damage);
+            Director.Construct(ref bulletBuilder, gameObject.id, player.damage, player.damage);
             return bulletBuilder.GetResult() as BulletObject;
         }
 
         public override CollectibleObject CreateCollectibleObject()
         {
             CollectibleBuilder collectibleBuilder = new CollectibleBuilder(new float[] { 0, 0 });
-            Director.director.Construct(ref collectibleBuilder, GameComponents.Collectible.CollectibleType.col1);
+            Director.Construct(ref collectibleBuilder, GameComponents.Collectible.CollectibleType.col1);
             return collectibleBuilder.GetResult() as CollectibleObject;
         }
     }
